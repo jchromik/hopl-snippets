d2a972fd7869a867a5913ab6b65851f6f8386836
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-05-03 15:44:21 +0200

    rewrite parprimes

--- parprimes.erl
+++ parprimes.erl
@@ -2,50 +2,32 @@
 -import(primes, [list_primes/3]).
 -compile(export_all).
 
-prime_gen_loop() ->
+generator(Merger, Begin, End, Step) ->
+      Merger ! primes:list_primes(Begin, End, Step).
+
+merger(Counter, Max, List) ->
+  merger(Counter, Max, List, os:timestamp()).
+
+merger(Counter, Max, List, StartTime) ->
   receive
-    {From, exit} ->
-      From ! exited;
-    {Merger, {Begin, End, Step}} ->
-      io:format("GENERATOR: got {~p, {~p, ~p, ~p}}~n", [Merger, Begin, End, Step]),
-      Merger ! {self(), primes:list_primes(Begin, End, Step)},
-      prime_gen_loop()
+    Result when Counter >= Max-1 ->
+      io:format("Result: ~p~n", [List ++ Result]),
+      io:format("Duration: ~p~n", [timer:now_diff(os:timestamp(), StartTime)]);
+    Result ->
+      merger(Counter+1, Max, List ++ Result, StartTime)
   end.
 
-prime_merge_loop(Counter, Max, List) ->
-  receive
-    {From, exit} ->
-      From ! exited;
-    {Generator, Result} when Counter >= Max-1 ->
-      Generator ! ok,
-      io:format("Result: ~p~n", [List ++ Result]);
-    {Generator, Result} ->
-      io:format("MERGER: got {~p, ~p}~n", [Generator, Result]),
-      Generator ! ok,
-      prime_merge_loop(Counter+1, Max, List ++ Result)
-  end.
+start_merger(Num) ->
+  spawn(parprimes, merger, [0, Num, []]).
 
-spawn_generators(Num) -> spawn_generators_helper(Num, []).
+start_generators(Merger, Begin, End, Num) ->
+  start_generators(Merger, Begin, End, Num, 0).
 
-spawn_generators_helper(0, Pids) -> Pids;
-spawn_generators_helper(Num, Pids) ->
-  io:format("spawning generator ~p~n", [length(Pids)+1]),
-  Fun = fun() -> prime_gen_loop() end,
-  Pid = spawn(Fun),
-  spawn_generators_helper(Num-1, [ Pid | Pids ]).
-
-spawn_merger(Num) ->
-  io:format("spawning merger~n"),
-  Fun = fun() -> prime_merge_loop(0, Num, []) end,
-  spawn(Fun).
-
-start_generators(_, _, [], _, _) -> ok;
-start_generators(Begin, End, [Generator | Rest], Merger, Num) ->
-  io:format("starting generator ~p~n", [Num - length(Rest)]),
-  Generator ! {Merger, {Begin + length(Rest), End, Num}},
-  start_generators(Begin, End, Rest, Merger, Num).
+start_generators(_, _, _, Num, Num) -> ok;
+start_generators(Merger, Begin, End, Num, Counter) ->
+  spawn(parprimes, generator, [Merger, Begin+Counter, End, Num]),
+  start_generators(Merger, Begin, End, Num, Counter+1).
 
 start(Begin, End, Num) ->
-  Generators = spawn_generators(Num),
-  Merger = spawn_merger(Num),
-  start_generators(Begin, End, Generators, Merger, Num).
+  Merger = start_merger(Num),
+  start_generators(Merger, Begin, End, Num).
