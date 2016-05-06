25290013913b3b2b16f10046cafac2ddb7f1e4a8
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-05-06 22:43:58 +0200

    add refactored benchmarking

--- primesbenchmark.erl
+++ primesbenchmark.erl
@@ -1,2 +1,19 @@
 -module(primesbenchmark).
--export([]).
+-import(parprimes, [start/3]).
+-import(seqprimes, [start/2]).
+-export([parseqbenchmark/2]).
+
+parseqbenchmark(Max,Step) ->
+  io:format("N;parallel;sequential~n"),
+  parseqbenchmark(0,Max,Step).
+
+parseqbenchmark(Curr,Max,_) when Curr > Max -> ok;
+parseqbenchmark(Curr,Max,Step) ->
+  TP1 = os:timestamp(),
+  parprimes:start(0,Curr,11),
+  TP2 = os:timestamp(),
+  TS1 = os:timestamp(),
+  seqprimes:start(0,Curr),
+  TS2 = os:timestamp(),
+  io:format("~p;~p;~p~n", [Curr,timer:now_diff(TP2,TP1),timer:now_diff(TS2,TS1)]),
+  parseqbenchmark(Curr+Step,Max,Step).
--- seqprimes.erl
+++ seqprimes.erl
@@ -1,6 +1,6 @@
 -module(seqprimes).
 -import(primes, [list_primes/2]).
--export([benchmark/2]).
+-export([start/2]).
 
 start(Begin, End) ->
   primes:list_primes(Begin, End).
