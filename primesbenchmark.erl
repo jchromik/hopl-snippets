c4559a8161e787154f63ad9365f49945f12d8951
Jonas Chromik <Jonas.Chromik@student.hpi.uni-potsdam.de>
2016-05-12 00:51:31 +0200

    benchmarking

--- primesbenchmark.erl
+++ primesbenchmark.erl
@@ -1,19 +1,43 @@
 -module(primesbenchmark).
 -import(parprimes, [start/3]).
 -import(seqprimes, [start/2]).
--export([parseqbenchmark/2]).
+-export([parseqbenchmark/3]).
 
-parseqbenchmark(Max,Step) ->
-  io:format("N;parallel;sequential~n"),
-  parseqbenchmark(0,Max,Step).
+printheader(Num) ->
+  io:format("N"),
+  printheader(0,Num,"parallel"),
+  printheader(0,Num,"sequential"),
+  io:format("~n").
 
-parseqbenchmark(Curr,Max,_) when Curr > Max -> ok;
-parseqbenchmark(Curr,Max,Step) ->
+printheader(Num,Num,_) -> ok;
+printheader(Curr,Num,Text) ->
+  io:format(";" ++ Text ++ "~p",[Curr]),
+  printheader(Curr+1,Num,Text).
+
+parseqbenchmark(Max,Step,Num) ->
+  printheader(Num),
+  parseqbenchmark(0,Max,Step,Num).
+
+parseqbenchmark(Curr,Max,_,_) when Curr > Max -> ok;
+parseqbenchmark(Curr,Max,Step,Num) ->
+  io:format("~p",[Curr]),
+  printParTimesArray(Curr,0,Num),
+  printSeqTimesArray(Curr,0,Num),
+  io:format("~n"),
+  parseqbenchmark(Curr+Step,Max,Step,Num).
+
+printParTimesArray(_,Num,Num) -> ok;
+printParTimesArray(N,Curr,Num) ->
   TP1 = os:timestamp(),
-  parprimes:start(0,Curr,11),
+  parprimes:start(0,N,11),
   TP2 = os:timestamp(),
+  io:format(";~p", [timer:now_diff(TP2,TP1)]),
+  printParTimesArray(N,Curr+1,Num).
+
+printSeqTimesArray(_,Num,Num) -> ok;
+printSeqTimesArray(N,Curr,Num) ->
   TS1 = os:timestamp(),
-  seqprimes:start(0,Curr),
+  seqprimes:start(0,N),
   TS2 = os:timestamp(),
-  io:format("~p;~p;~p~n", [Curr,timer:now_diff(TP2,TP1),timer:now_diff(TS2,TS1)]),
-  parseqbenchmark(Curr+Step,Max,Step).
+  io:format(";~p", [timer:now_diff(TS2,TS1)]),
+  printSeqTimesArray(N,Curr+1,Num).
